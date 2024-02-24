016b42e08a4fe5004ace9005d693da75f7a38cb7
virginrobotics <neverofftheinternet@gmail.com>
2024-02-24 23:20:45 +0530

    bad 1hz pulser

--- shift_uart/top.v
+++ shift_uart/top.v
@@ -3,7 +3,10 @@
 module top (
     input   hwclk,
     input   pb,
-    output  ftdi_tx
+    output  ftdi_tx,
+    //output  frame_sent,
+    output  led2,
+    output  led1
 );
     // Converting a 12Mhz source clock to a 9600 baud for UART
     parameter CNTR_W = 32;
@@ -21,6 +24,7 @@ module top (
     reg [FRAME_WIDTH-1:0] data_frame = 8'b01001000;
     
     wire baud_clk;
+    wire one_hz;
 
     //9600 Hz UART baud clk generator
 
@@ -36,6 +40,30 @@ module top (
     );
 
 
+    // One Hz send_en generator
+    baud_gen #(
+    .CNTR_W(CNTR_W),
+    .SOURCE_CLK(SOURCE_CLK),
+    .TARGET_CLK(TARGET_CLK1HZ)
+    ) baud_gen_one_hz (
+        // ref clk
+        .hwclk(hwclk),
+        // baud clk out
+        .baud_clk(one_hz)
+    );
+
+    // Send enable pulse generator from 1 Hz clock
+    reg one_hz_reg;
+    wire send_en_pulse;
+    always @(posedge baud_clk) begin
+        one_hz_reg <= one_hz;        
+    end
+
+    assign send_en_pulse = one_hz & !one_hz_reg;
+
+    assign led1 = one_hz;
+    assign led2 = frame_sent;
+
     // Shift register based basic UART 8n1 transmitter
 
     shift_tx_fsm #(
@@ -43,7 +71,7 @@ module top (
         .SHIFT_AMOUNT(SHIFT_AMOUNT)
     ) uart_tx_inst (
         .baud_clk(baud_clk),
-        .send_en(1'b1),
+        .send_en(send_en_pulse),
         .data_frame(data_frame),
         .ftdi_tx(ftdi_tx),
         .frame_sent(frame_sent)
